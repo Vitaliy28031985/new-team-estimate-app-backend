9a787d6f3ece7aae2313485becfc38d50022b70a
Vitaliy <94307584+Vitaliy28031985@users.noreply.github.com>
2024-12-05 13:37:15 +0200

    Update middle.prices.service.ts

--- src/middle-prices/middle.prices.service.ts
+++ src/middle-prices/middle.prices.service.ts
@@ -3,7 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model, Types } from 'mongoose';
 import { MiddlePrice } from 'src/mongo/schemas/middle-prices/middle.prices';
 import { PricesDto } from 'src/prices/price.dto';
-import { Helpers } from 'src/projects/positions/helpers';
+// import { Helpers } from 'src/projects/positions/helpers';
 
 @Injectable()
 export class MiddlePricesService {
@@ -15,39 +15,39 @@ export class MiddlePricesService {
     return await this.middlePriceModel.find();
   }
 
-  async addMiddlePrice(dto: PricesDto) {
-    const middlePrices = await this.middlePriceModel.find({
-      title: dto.title,
-    });
+  // async addMiddlePrice(dto: PricesDto) {
+  //   const middlePrices = await this.middlePriceModel.find({
+  //     title: dto.title,
+  //   });
 
-    if (middlePrices.length === 0) {
-      await this.middlePriceModel.create({
-        title: dto.title,
-        price: dto.price,
-        prices: [{ id: dto.id, price: dto.price, owner: dto.owner }],
-      });
-    }
-    if (middlePrices.length !== 0) {
-      await this.middlePriceModel.findByIdAndUpdate(
-        middlePrices[0]._id,
-        {
-          $push: {
-            prices: { id: dto.id, price: dto.price, owner: dto.owner },
-          },
-        },
-        { new: true },
-      );
-      const newMiddlePrices = await this.middlePriceModel.find({
-        title: dto.title,
-      });
+  //   if (middlePrices.length === 0) {
+  //     await this.middlePriceModel.create({
+  //       title: dto.title,
+  //       price: dto.price,
+  //       prices: [{ id: dto.id, price: dto.price, owner: dto.owner }],
+  //     });
+  //   }
+  //   if (middlePrices.length !== 0) {
+  //     await this.middlePriceModel.findByIdAndUpdate(
+  //       middlePrices[0]._id,
+  //       {
+  //         $push: {
+  //           prices: { id: dto.id, price: dto.price, owner: dto.owner },
+  //         },
+  //       },
+  //       { new: true },
+  //     );
+  //     const newMiddlePrices = await this.middlePriceModel.find({
+  //       title: dto.title,
+  //     });
 
-      await this.middlePriceModel.findByIdAndUpdate(
-        newMiddlePrices[0]._id,
-        { $set: { price: Helpers.middlePrice(newMiddlePrices[0].prices) } },
-        { new: true },
-      );
-    }
-  }
+  //     await this.middlePriceModel.findByIdAndUpdate(
+  //       newMiddlePrices[0]._id,
+  //       { $set: { price: Helpers.middlePrice(newMiddlePrices[0].prices) } },
+  //       { new: true },
+  //     );
+  //   }
+  // }
 
   // async updateMiddlePrice(dto: PricesDto) {
   //   const middlePrices = await this.middlePriceModel.find();
